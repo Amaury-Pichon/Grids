f6f348359fb45d39dc23a5df66381dc5cc4538dd
Neopick <neopick@hotmail.com>
2020-03-31 00:07:36 +0200

    Modification du layout des paramètres et de l'intent renvoyé en fonction de l'intent reçu.

--- app/src/main/java/com/neopick/soundboard/ButtonSettingActivity.java
+++ app/src/main/java/com/neopick/soundboard/ButtonSettingActivity.java
@@ -19,26 +19,38 @@ public class ButtonSettingActivity extends AppCompatActivity {
     private Intent incomingIntent;
     Intent leavingIntent = new Intent();
     private int bgColor = Color.WHITE;
+    EditText etButtonSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_button_setting);
+        incomingIntent  = getIntent();
+
+        if(incomingIntent.getIdentifier().equals("SoundButton")){
+            setContentView(R.layout.activity_button_sound_setting);
+            TextView tvButtonSource = findViewById(R.id.textView_source);
+            tvButtonSource.setText("URL du son");
+            etButtonSource = findViewById(R.id.editText_source);
+            etButtonSource.setHint("Tapez une URL pour le son");
+        }
+        else if(incomingIntent.getIdentifier().equals("CountButton")){
+            setContentView(R.layout.activity_button_setting);
+        }
 
         this.setFinishOnTouchOutside(false);
 
-        incomingIntent  = getIntent();
+
         bgColor = incomingIntent.getIntExtra("LAST_BG_COLOR", Color.WHITE);
 
         setTitle("Paramètres");
 
         TextView tvButtonName = findViewById(R.id.textView_button_name);
         final EditText etButtonName = findViewById(R.id.editText_button_name);
-        Button buttonCancel = findViewById(R.id.button_cancel_add);
-        Button buttonOk = findViewById(R.id.button_ok);
         TextView tvButtonBackground = findViewById(R.id.textView_bg_color);
         ColorPickerView colorPicker = findViewById(R.id.setting_colorPicker);
         final View preview = findViewById(R.id.view_preview);
+        Button buttonCancel = findViewById(R.id.button_cancel_add);
+        Button buttonOk = findViewById(R.id.button_ok);
 
 
         tvButtonName.setText("Nom (8 caractères max) : ");
@@ -62,7 +74,7 @@ public class ButtonSettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 leavingIntent.putExtra("BG_COLOR", bgColor);
-                setResult(0, leavingIntent);
+                setResult(RESULT_CANCELED, leavingIntent);
                 finish();
             }
         });
@@ -72,9 +84,14 @@ public class ButtonSettingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String buttonName = etButtonName.getText().toString();
                 leavingIntent.putExtra("BG_COLOR", bgColor);
+
+                if(incomingIntent.getIdentifier().equals("SoundButton")){
+                    String soundURL = etButtonSource.getText().toString();
+                    leavingIntent.putExtra("SOUND_SOURCE", soundURL);
+                }
                 if(buttonName.length() <= 8) {
                     leavingIntent.putExtra("BUTTON_NAME", buttonName);
-                    setResult(1, leavingIntent);
+                    setResult(RESULT_OK, leavingIntent);
                     finish();
                 }
                 else{
